919ec7be45345c282990cedd6d1f795ea0745149
mateus gomes <mateusgomesc04@gmail.com>
2023-06-30 18:58:06 -0300

    feat: creating Home page screen with styles

---
+++ src/components/ListHistoric/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Icon from "react-native-vector-icons/Feather";
+
+import {Container, Tipo, IconView, TipoText, ValorText} from "./styles"
+export default function HistoricoList({data}){
+    return (
+        <Container>
+            <Tipo>
+                <IconView tipo={data.tipo}>
+                    <Icon name={data.tipo === 'despesa' ? 'arrow-down' : 'arrow-up'} color="#FFF" size={20}/>
+                    <TipoText>{data.tipo}</TipoText>
+                </IconView>
+            </Tipo>
+            <ValorText>R$ {data.valor.toFixed(2)}</ValorText>
+        </Container>
+    )
+} 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,37 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { View, Text, Button } from "react-native";
 import { AuthContext } from "../../contexts/auth";
 
-export default function Home(){
-    const {signOut} = useContext(AuthContext)
+import Header from "../../components/Header";
+import { Background, Container, Saldo, Title, Nome, List } from "./styles"
+import HistoricoList from "../../components/ListHistoric";
+
+export default function Home() {
+    const { user, signOut } = useContext(AuthContext)
+
+    const [historico, setHistorico] = useState([
+        { key: '1', tipo: 'receita', valor: 1500 },
+        { key: '2', tipo: 'despesa', valor: 250 },
+        { key: '3', tipo: 'receita', valor: 45 },
+        { key: '4', tipo: 'despesa', valor: 85.25 },
+    ])
 
     return (
-        <View>
-            <Text>Home</Text>
-            <Button 
-            title="Sign Out account"
-            onPress={() => signOut()}
+        <Background>
+            <Header />
+            <Container>
+                <Nome>Everson</Nome>
+                <Saldo>R$ 100,99</Saldo>
+            </Container>
+
+            <Title>Ultimas movimentações</Title>
+
+            <List
+                showsVerticalScrollIndicator={false}
+                data={historico}
+                keyExtractor={item => item.key}
+                renderItem={({ item }) => (<HistoricoList data={item} />)}
             />
-        </View>
+            </Background>
     )
 }
